2d3564360a5d34cabf052c562bca4a63bb5c6165
IanTapply22 <iantapply22@gmail.com>
2026-08-09 15:29:50 -0400

    chore: bump dependency versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ subprojects {
     }
 
     dependencies {
-        "testImplementation"(platform("org.junit:junit-bom:6.0.1"))
+        "testImplementation"(platform("org.junit:junit-bom:6.1.2"))
         "testImplementation"("org.junit.jupiter:junit-jupiter")
         "testRuntimeOnly"("org.junit.platform:junit-platform-launcher")
     }
